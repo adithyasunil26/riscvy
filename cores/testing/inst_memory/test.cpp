1766f3a6dca85ebef0514938cb423237183e9a58
adithyasunil26 <adithyasunil26@gmail.com>
2024-03-17 18:38:50 +0530

    inst_memory

---
+++ cores/testing/inst_memory/test.cpp
@@ -0,0 +1,52 @@
+#include <verilated.h>
+#include <verilated_vcd_c.h>
+
+#include "Vinst_memory.h" //Change module name here
+
+using namespace std;
+
+#define MAX_TIME 0.23 //Set max time here in ns 
+#define PERIOD 10  //Set simulation period here in ps
+static Vinst_memory* ptop = new Vinst_memory;  //Change module name here
+
+vluint64_t sim_time = 0;
+static VerilatedVcdC* tfp = new VerilatedVcdC;
+
+static void tick(int count)
+{
+  for (;count > 0; --count)
+  {
+    ptop->eval();
+    sim_time++;
+    tfp->dump(sim_time);
+  }
+}
+
+static void run(uint64_t limit)
+{
+  uint64_t count = 0;
+
+  while(count < limit)
+  {
+    ptop->inst_address = count*4;   //Modify as per module
+    tick(PERIOD);                 //
+    ++count;
+  }
+}
+
+
+int main(int argc, char** argv, char** env) {
+  Verilated::commandArgs(argc, argv);
+  Verilated::traceEverOn(true);
+
+  ptop->trace(tfp, 99);
+  tfp->open("dump.vcd");
+
+  run(MAX_TIME*1000/PERIOD);
+
+  tfp->close();
+  ptop->final();
+  delete tfp;
+  delete ptop;
+  return 0;
+}
