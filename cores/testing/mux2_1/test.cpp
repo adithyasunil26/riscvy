b648104686ca681c455a8fccd13d76583de99030
adithyasunil26 <adithyasunil26@gmail.com>
2024-03-17 18:21:25 +0530

    mux2_1 test

---
+++ cores/testing/mux2_1/test.cpp
@@ -0,0 +1,56 @@
+#include <verilated.h>
+#include <verilated_vcd_c.h>
+
+#include "Vmux2_1.h" //Change module name here
+
+using namespace std;
+
+#define MAX_TIME 10 //Set max time here in ns 
+#define PERIOD 10  //Set simulation period here in ps
+static Vmux2_1* ptop = new Vmux2_1;  //Change module name here
+
+vluint64_t sim_time = 0;
+static VerilatedVcdC* tfp = new VerilatedVcdC;
+
+static void tick(int count)
+{
+  for (;count > 0; --count)
+  {
+    ptop->eval();
+    sim_time++;
+    tfp->dump(sim_time);
+  }
+}
+
+static void run(uint64_t limit)
+{
+  uint64_t count = 0;
+
+  while(count < limit)
+  {
+    ptop->a = count;      //Modify as per module
+    ptop->b = count+1;    //
+    ptop->sel = 1;        //
+    tick(PERIOD/2);       //
+    ptop->sel = 0;        //
+    tick(PERIOD/2);       //
+    ++count;
+  }
+}
+
+
+int main(int argc, char** argv, char** env) {
+  Verilated::commandArgs(argc, argv);
+  Verilated::traceEverOn(true);
+
+  ptop->trace(tfp, 99);
+  tfp->open("dump.vcd");
+
+  run(MAX_TIME*1000/PERIOD);
+
+  tfp->close();
+  ptop->final();
+  delete tfp;
+  delete ptop;
+  return 0;
+}
